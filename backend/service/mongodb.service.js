ad400bb96013c9911f3f2194757ab7f786cd16cb
Atmaak <77574694+Atmaak@users.noreply.github.com>
2024-04-15 17:18:12 +0200

    x

--- backend/service/mongodb.service.js
+++ backend/service/mongodb.service.js
@@ -1,21 +1,22 @@
 const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = `mongodb+srv://atmaak:EAzZd9q2DboWKZiz@vecerniskola.vv90o2l.mongodb.net/?retryWrites=true&w=majority&appName=vecerniSkola`
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri)
+const username = encodeURIComponent("atmaak");
+const password = encodeURIComponent("kokot");
+const uri = `mongodb+srv://${username}:${password}@tomasprojekt.l4xpyze.mongodb.net/?retryWrites=true&w=majority&appName=TomasProjekt`;
 
-// async function run() {
-//   try {
-//     const database = client.db('vecerniSkola')
-//     const users = database.collection('users')
-//     users.insertOne({
-//       username: "atmaak",
-//       password: "xdmoment"
-//     })
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close()
-//   }
-// }
-// run().catch(console.dir)
+const client = new MongoClient(uri);
 
-module.exports = client.db('vecerniSkola')
+async function init() {
+  try {
+    await client.connect();
+    await client.db("calendar").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // await client.close();
+  }
+}
+
+init()
+
+module.exports = {
+  client
+};
